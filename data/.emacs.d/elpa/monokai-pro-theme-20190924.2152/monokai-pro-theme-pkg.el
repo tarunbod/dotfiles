f533669cec5b5d310aa942a3d5fcc22d8148fea1
Tarun Boddupalli <tarunbod@gmail.com>
2022-11-12 16:43:19 -0500

    Deleted emacs config (vim supremacy)

--- data/.emacs.d/elpa/monokai-pro-theme-20190924.2152/monokai-pro-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "monokai-pro-theme" "20190924.2152" "A simple theme based on the Monokai Pro Sublime color schemes" 'nil :commit "b5dcc197cf36b181362b468da48b67a5f2199cae" :authors '(("Kaleb Elwert" . "kaleb@coded.io")) :maintainer '("Kaleb Elwert" . "kaleb@coded.io") :url "https://github.com/belak/emacs-monokai-pro-theme")
